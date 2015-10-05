c21e4c0e9ad42fc7fa5e7c2d749a26c877e56915
Braden <braden337@gmail.com>
2015-10-05 21:50:53 +0000

    try some async stuff

--- app.rb
+++ app.rb
@@ -5,19 +5,19 @@ set :bind, '0.0.0.0'
 
 
 
-basesarr = [
-            {name: "Shop", url: "http://205.200.66.50/"},
-            {name: "McGillivray", url: "http://66.244.213.166/"},
-            {name: "La Salle", url: "http://64.141.35.142/"},
-            {name: "Roblin", url: "http://206.45.135.119:81/"},
-            {name: "Petersfield", url: "http://206.45.204.94/"},
-            {name: "Interlake", url: "http://199.19.62.91/"},
-            {name: "Riverton", url: "http://104.153.49.53:81/"},
-            {name: "Fisher Branch", url: "http://104.218.46.149:81/"},
+bases = [
+            {name: "Shop", url: "http://205.200.66.50/", status: nil},
+            {name: "McGillivray", url: "http://66.244.213.166/", status: nil},
+            {name: "La Salle", url: "http://64.141.35.142/", status: nil},
+            {name: "Roblin", url: "http://206.45.135.119:81/", status: nil},
+            {name: "Petersfield", url: "http://206.45.204.94/", status: nil},
+            {name: "Interlake", url: "http://199.19.62.91/", status: nil},
+            {name: "Riverton", url: "http://104.153.49.53:81/", status: nil},
+            {name: "Fisher Branch", url: "http://104.218.46.149:81/", status: nil},
            ]
 
 def checkStatus(bases)
-    outArr = []
+    # outArr = []
     bases.each do |base|
         # found lots of info at
         # http://opensourceconnections.com/blog/2008/04/24/adding-timeout-to-nethttp-get_response/
@@ -31,17 +31,16 @@ def checkStatus(bases)
           end
           #puts resp.kind_of? Net::HTTPResponse
           #puts resp.code
-          outArr << {name: base[:name], url: base[:url], status: "success"} #if resp.code.to_i == 401
+          base[:status] = "success" #if resp.code.to_i == 401
         rescue => e
           #puts e
-          outArr << {name: base[:name], url: base[:url], status: "danger"}
+          base[:status] = "danger"
         end #close begin
     end #close bases.each
-    outArr
 end
 
 get '/' do
-  @bases = checkStatus(basesarr)
+  @bases = bases
   erb :index
 end
 
